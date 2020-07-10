f762576d68a40942b1f66cfe50391ac2254e540b
Colin LaMont <colinharrylamont@gmail.com>
2020-07-10 22:11:23 +0200

    Rejection sampling, sparse fitness struct, and reducing allocations in Birth events.

--- src/genetics.jl
+++ src/genetics.jl
@@ -13,21 +13,51 @@ mutable struct Population{T}		# parameters defining a population state
 	birth_total::T			
 end
 
+struct FitVector # Custom sparse array type for fitness vector stuff
+	sind::Array{Int64,1} # sites under selection
+	nind::Array{Int64,1} # neutral sites
+	svals::Array{Float64,1} # fitness addition, if mutant
+	βwt::Float64 # β0 + sum(β1), the wildtype fitness (zero bitvector)
+	βmax::Float64 # β0 + sum(abs.(a)) the maximum fitness
+end
 
-@with_kw struct PopRates{R}		# parameters defining a population
-	κ::R 				= 1000.		# avg pop size
-	β0::R 				= 1.0		# base (competetive) fitness  
-	σ::R 				= 0.5 		# excess Poisson noise
-	μ::R 				= 1.0e-3	# mutation rate per site
-	χ::R				= 0.0		# outcrossing rate (between 0 and one), rate of recombination events
-	ρ::R 				= 1.0e-2	# how tightly two crossed genomes get wound together (# of crosses per nucleotide)
-	f::Function 		= (x->0.0)	# mapping from genotype to fitness (increased birth + competion)
+function FitVector(a::Array, β0::Float64)
+	# vector of fitness differences between wildtype and mutant
+	FitVector(
+		findall(!iszero, a),
+		findall(iszero, a),
+		-a[findall(!iszero, a)],
+		β0 + sum(a)/2,
+		β0 + sum(abs.(a))/2
+	)
+end
+
+function fitness(f::FitVector, x::BitArray)
+	s = 0
+	for (ii,val) in zip(f.sind,f.svals)
+		if x[ii]
+			s += val
+		end
+	end
+	return s + f.βwt
 end
 
 
-function PopRates(a::Array)
-	PopRates(;f=(x->sum(a[ii]x[ii] for ii in eachindex(a))))
+##
+@with_kw struct PopRates		# parameters defining a population
+	loci::Int64				= 2^8
+	κ::Float64 				= 1000.		# avg pop size
+	β0::Float64 			= 1.0		# base (competetive) fitness  
+	β1::Array{Float64,1}	= zeros(loci)	# fitness difference between sites
+	βf::FitVector			= FitVector(β1,β0)
+	σ::Float64				= 0.5 		# excess Poisson noise
+	μ::Float64 				= 1.0e-3	# mutation rate per site
+	χ::Float64				= 0.0		# outcrossing rate (between 0 and one), rate of recombination events
+	ρ::Float64 				= 1.0e-2	# how tightly two crossed genomes get wound together (# of crosses per nucleotide)
+	f::Function 			= (x->fitness(βf,x))	# mapping from genotype to fitness (increased birth + competion)
+	# if you want epistasis, you can always code it in by hand in this function.
 end
+
 
 ## Statistics on PopRates
 
@@ -74,9 +104,21 @@ end
 function stat_D(pop::Population; 
 	sites = 1:length(pop.individuals[1]), # vector of sites to use in likelihood 
 	k = length(pop.individuals)) # size of population to take counts one, defaults to all
-		vec = sum(sample(pop.individuals, k, replace=false))
+		vec = sum(sample(pop.individuals, k, replace=false))[sites]
 		vec = permutedims(hcat(vec , k .- vec))
 		neutral_D(vec)
+end
+
+"""
+If fitnesses are known, we calculate diversity only from the neutral sites.
+"""
+function stat_D(pop::Population, par::PopRates; 
+	k = length(pop.individuals)) # size of population to take counts one, defaults to all
+	stat_D(pop, sites = par.βf.nind, k = k)
+end
+
+function stat_freq(pop::Population)
+	mean(pop.individuals)
 end
 
 function stat_fit(pop::Population)
@@ -106,10 +148,6 @@ function neutral_wright_sample(par::PopRates)
 	random_wright_sample(pop_D(par), 1//2, 0)
 end
 
-
-
-
-
 """
 Construct a population individuals with site entries equal to 1 according to binomial
 frequencies 
@@ -117,7 +155,7 @@ frequencies
 function initialize_pop(frequencies::Vector, pop_size::Int, par::PopRates)
     genomes = mapreduce(p->rand(Bernoulli(p), pop_size),hcat,frequencies)
     individuals = [BitArray(genomes[ii,:]) for ii in 1:pop_size]
-    fitness = par.β0 .+ par.f.(individuals)
+    fitness = par.f.(individuals)
     birth = fitness .+ par.σ
     birth_total = sum(birth)
     death =  par.σ .+ sum(fitness)/par.κ
@@ -131,20 +169,21 @@ function initialize_pop(frequencies::Vector, pop_size::Int, par::PopRates)
 end
 
 # asumption of neutrality
-function initialize_pop(par::PopRates; gene_size=128, pop_size = ceil(Int, par.κ))
-initialize_pop([neutral_wright_sample(par::PopRates) for ii in 1:gene_size], pop_size, par)
+function initialize_pop(par::PopRates; pop_size = ceil(Int, par.κ))
+initialize_pop([neutral_wright_sample(par::PopRates) for ii in 1:par.loci], pop_size, par)
 end
 
 # simpler specification for constant frequencies
-function initialize_pop(freq::Real, par::PopRates; gene_size = 128, pop_size = ceil(Int, par.κ))
-initialize_pop([freq for ii in 1:gene_size], pop_size, par)
+# useful for indicating pure wildtype
+function initialize_pop(freq::Real, par::PopRates; pop_size = ceil(Int, par.κ))
+initialize_pop([freq for ii in 1:par.loci], pop_size, par)
 end
 
 """
 Recalculate the caches
 """
 function renew_fitness(pop::Population, par::PopRates)
-	pop.fitness = par.β0 .+ par.f.(pop.individual)
+	pop.fitness = par.f.(pop.individual)
 	pop.birth = pop.fitness .+ par.σ
 	pop.death = sum(pop.fitness)/par.κ .+ par.σ
 	pop.size = length(pop.individual)
